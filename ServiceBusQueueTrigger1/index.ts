b83188612e3bc926b9250ccab1028af75e8b517b
Ryan Holmes <ryan.holmes@stsaviationgroup.com>
2020-12-22 12:22:15 -0500

    init commit

---
+++ ServiceBusQueueTrigger1/index.ts
@@ -0,0 +1,10 @@
+import { AzureFunction, Context } from "@azure/functions";
+
+const serviceBusQueueTrigger: AzureFunction = async function (
+  context: Context,
+  mySbMsg: any
+): Promise<void> {
+  context.log("ServiceBus queue trigger function processed message", mySbMsg);
+};
+
+export default serviceBusQueueTrigger;
